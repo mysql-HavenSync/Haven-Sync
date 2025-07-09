5d9d72752db84dfef12ffb436576ae9a3ea61d76
Hexahaven <your-email@example.com>
2025-07-09 15:10:51 +0530

    fix crashed device route

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
 
-const deviceRoutes = require('./routes/deviceRoutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
